1d06005a3927c08fc5174212784b9169b396742a
Puspita <Rahayupuspita571@gmail.com>
2017-03-16 23:17:12 +0700

    aplikasi ini dapat melakukan pendataan pengunjung yang akan menyewa motor, dan data akan dimasukkan di database

---
+++ src/uts_xir2_pbo/UTS_XIR2_PBO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_xir2_pbo;
+
+/**
+ *
+ * @author Puspita
+ */
+public class UTS_XIR2_PBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
